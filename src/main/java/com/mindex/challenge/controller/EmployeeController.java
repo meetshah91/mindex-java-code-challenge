93754f564006f8642f398bdb816211c67e8dfcf7
Meet Shah <ms9461@g.rit.edu>
2022-12-18 00:09:40 -0500

    Created numberOfReports and Compensation Restful Services and Added Test classes.

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,30 +10,31 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
-    private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
-    @Autowired
-    private EmployeeService employeeService;
+	@Autowired
+	private EmployeeService employeeService;
 
-    @PostMapping("/employee")
-    public Employee create(@RequestBody Employee employee) {
-        LOG.debug("Received employee create request for [{}]", employee);
+	@PostMapping("/employee")
+	public Employee create(@RequestBody Employee employee) {
+		LOG.debug("Received employee create request for [{}]", employee);
 
-        return employeeService.create(employee);
-    }
+		return employeeService.create(employee);
+	}
 
-    @GetMapping("/employee/{id}")
-    public Employee read(@PathVariable String id) {
-        LOG.debug("Received employee create request for id [{}]", id);
+	@GetMapping("/employee/{id}")
+	public Employee read(@PathVariable String id) {
+		LOG.debug("Received employee create request for id [{}]", id);
 
-        return employeeService.read(id);
-    }
+		return employeeService.read(id);
+	}
 
-    @PutMapping("/employee/{id}")
-    public Employee update(@PathVariable String id, @RequestBody Employee employee) {
-        LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
+	@PutMapping("/employee/{id}")
+	public Employee update(@PathVariable String id, @RequestBody Employee employee) {
+		LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
 
-        employee.setEmployeeId(id);
-        return employeeService.update(employee);
-    }
+		employee.setEmployeeId(id);
+		return employeeService.update(employee);
+	}
+
 }
